4183a5a6eacca7d37bdd4607806693c2175405b1
shaheryar <shaheryarsherry114@gmail.com>
2024-07-04 16:18:30 +0700

    again changes

--- src/data/dataLists.js
+++ src/data/dataLists.js
@@ -40,74 +40,74 @@ let Products = [
   {
     id: 7,
     name: "Chicken Cheese",
-    photo: "/images/pizza1.jpg",
+    photo: "/image/pizza1.jpg",
     price: 750
   },
   {
     id: 8,
     name: "Mac and Cheese ",
-    photo: "/images/pizza11.jpeg",
+    photo: "/image/pizza11.jpeg",
     price: 850,
   },
   {
     id: 9,
     name: "Supreme pizza",
-    photo: "/images/pizza14.jpg",
+    photo: "/image/pizza14.jpg",
     price: 760,
   },
   {
     id: 10,
     name: "GO food-Special",
-    photo: "/images/pizza13.jpeg",
+    photo: "/image/pizza13.jpeg",
     price: 750
   },
 
   {
     id: 11,
     name: "Chicken Tika Pizza",
-    photo: "/images/pizz12.jpeg",
+    photo: "/image/pizz12.jpeg",
     price:  750,
   },
   {
     id: 12,
     name: "Chicken Tika Pizza",
-    photo: "/images/pizza3.png",
+    photo: "/image/pizza3.png",
     price:  750,
   },
   {
     id: 13,
     name: "Penne pasta",
-    photo: "/images/pasta1.png",
+    photo: "/image/pasta1.png",
     price: 450,
   },
   {
     id: 14,
     name: "Ravioli pasta",
-    photo: "/images/pasta2.png",
+    photo: "/image/pasta2.png",
     price: 400,
   },
   {
     id: 15,
     name: "Linguine pasta",
-    photo: "/images/pasta3.png",
+    photo: "/image/pasta3.png",
     price: 550,
   },
   {
     id: 16,
     name: "Rigaton pasta",
-    photo: "/images/pasta4.png",
+    photo: "/image/pasta4.png",
     price: 500,
   },
   {
     id: 17,
     name: "Farfalle pasta",
-    photo: "/images/pasta5.png",
+    photo: "/image/pasta5.png",
     price: 650,
   },
   {
     id: 18,
     name: "Cannelloni pasta",
-    photo: "/images/pasta6.png",
+    photo: "/image/pasta6.png",
     price: 450,
   },
   {
@@ -125,25 +125,25 @@ let Products = [
   {
     id: 21,
     name: "Veggie Burger",
-    photo: "/images/burger7.png",
+    photo: "/image/burger7.png",
     price: 650,
   },
   {
     id: 22,
     name: "Chicken Nuggets",
-    photo: "/images/burger7.png",
+    photo: "/image/burger7.png",
     price: 450,
   },
   {
     id: 23,
     name: "Chicken Wrap",
-    photo: "images/burger 11.webp",
+    photo: "image/burger 11.webp",
     price: 550,
   },
   {
     id: 24,
     name: "Special Pasta",
-    photo: "/images/pasta3.png",
+    photo: "/image/pasta3.png",
     price: 700,
   },
   {
@@ -179,7 +179,7 @@ let Products = [
   {
     id: 30,
     name: "Chicken Pizza",
-    photo: "/Images/pizza-3007395_1280.jpg",
+    photo: "/Image/pizza-3007395_1280.jpg",
     price: 850,
   },
 ];
